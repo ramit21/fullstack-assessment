fb0bb220c5c0a2dee7d484b283681f9b942f1d97
21.ramit@gmail.com <21.ramit@gmail.com>
2020-04-29 14:50:43 +0530

    Employee Service - save employee

--- frontend/src/app/services/employee.service.spec.ts
+++ frontend/src/app/services/employee.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule,HttpTestingController } from '@angular/common/http/testing';
 import { EmployeeService } from './employee.service';
+import { Employee } from '../model/Bug';
 
 describe('EmployeeService', () => {
 
@@ -28,6 +29,20 @@ describe('EmployeeService', () => {
     
     let dataRequest = httpMock.expectOne('http://localhost:8080/employees');
     dataRequest.flush([{"id":4,"firstName":"Ciara","lastName":"Lavelle","gender":"F","dateOfJoining":"31/12/2014","dept":"HR"}]);
+    httpMock.verify();
+    
+  });
+
+  it('saveEmployee saves employee data', (done) => {
+    const employeeService : EmployeeService = TestBed.get(EmployeeService);
+    const emp = new Employee(null, "Ramit", "Sharma", "M", "31/12/2014", "IT");
+    employeeService.saveEmployee(emp).subscribe(res => {
+        expect(res).toEqual(1);
+        done(); 
+    });
+    
+    let dataRequest = httpMock.expectOne('http://localhost:8080/saveEmployee');
+    dataRequest.flush(1);
     httpMock.verify();
   });
 
--- frontend/src/app/services/employee.service.ts
+++ frontend/src/app/services/employee.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Employee } from '../model/Bug';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  private _url : string = "http://localhost:8080/employees"; 
+  private _url : string = "http://localhost:8080/"; 
 
   constructor(private _http: HttpClient) { }
 
   getEmployees(){
-    return this._http.get(this._url);
+    return this._http.get(this._url+'employees');
+  }
+
+  saveEmployee(employee: Employee){
+    return this._http.post(this._url+'saveEmployee', employee);
   }
 }
