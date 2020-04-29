708614805ed4adb22da2d146e40e06cd52972415
21.ramit@gmail.com <21.ramit@gmail.com>
2020-04-30 01:19:56 +0530

    additonal unittest cases

--- frontend/src/app/components/employee/employee.component.spec.ts
+++ frontend/src/app/components/employee/employee.component.spec.ts
@@ -3,6 +3,7 @@ import { EmployeeComponent } from './employee.component';
 import { EmployeeService } from '../../services/employee.service';
 import { Observable, of} from 'rxjs';
 import { FormsModule } from '@angular/forms';
+import { Employee } from 'src/app/model/Employee';
 
 describe('EmployeeComponent', () => {
   let component: EmployeeComponent;
@@ -26,10 +27,24 @@ describe('EmployeeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('getEmployees returns employee data', () => {
+    component.getEmployees();
+    expect(component.employees[0].firstName).toEqual('Ciara');
+  });
+
+  it('saveEmployees updates employee list with new employee', () => {
+    component.firstName = "Ciara";
+    component.createEmployee();
+    expect(component.employees[0].firstName).toEqual('Ciara');
+  });
 });
 
 class FakeEmployeeService{
-    getEmployees():Observable<any>{
-      return of('');
+  getEmployees():Observable<Employee[]>{
+      return of([{"id":4,"firstName":"Ciara","lastName":"Lavelle","gender":"F","dateOfJoining":"31/12/2014","dept":"HR"}]);
+  }
+  saveEmployee():Observable<any>{
+    return of({});
   }
 }
