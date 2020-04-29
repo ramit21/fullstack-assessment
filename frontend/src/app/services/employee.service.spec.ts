bba9f21aeb6f989fa3f17c13951162ade8abce35
21.ramit@gmail.com <21.ramit@gmail.com>
2020-04-29 14:40:31 +0530

    Employee Service - getall

--- frontend/src/app/services/employee.service.spec.ts
+++ frontend/src/app/services/employee.service.spec.ts
@@ -1,12 +1,35 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule,HttpTestingController } from '@angular/common/http/testing';
 import { EmployeeService } from './employee.service';
 
 describe('EmployeeService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  let httpMock : HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [EmployeeService],
+      imports: [HttpClientTestingModule] //HttpClient needed for the http in the constructor of the service
+    });
+    httpMock = TestBed.get(HttpTestingController);
+  });
 
   it('should be created', () => {
-    const service: EmployeeService = TestBed.get(EmployeeService);
+    const service : EmployeeService = TestBed.get(EmployeeService);
     expect(service).toBeTruthy();
   });
+
+  it('getEmployees returns employees data', (done) => {
+    const employeeService : EmployeeService = TestBed.get(EmployeeService);
+    employeeService.getEmployees().subscribe(res => {
+        expect(res).toEqual([{"id":4,"firstName":"Ciara","lastName":"Lavelle","gender":"F","dateOfJoining":"31/12/2014","dept":"HR"}]);
+        done(); 
+    });
+    
+    let dataRequest = httpMock.expectOne('http://localhost:8080/employees');
+    dataRequest.flush([{"id":4,"firstName":"Ciara","lastName":"Lavelle","gender":"F","dateOfJoining":"31/12/2014","dept":"HR"}]);
+    httpMock.verify();
+  });
+
+
 });
--- frontend/src/app/services/employee.service.ts
+++ frontend/src/app/services/employee.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  private _url : string = "http://localhost:8080/employees"; 
+
+  constructor(private _http: HttpClient) { }
+
+  getEmployees(){
+    return this._http.get(this._url);
+  }
 }
